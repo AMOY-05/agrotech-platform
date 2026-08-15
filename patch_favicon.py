499c0b0bba44c3775a20628026d270c170f314a6
AMOY <abdulmalikoyediran44@gmail.com>
2026-08-15 11:14:54 +0100

    Fix the icon

--- patch_favicon.py
+++ patch_favicon.py
@@ -1,7 +1,9 @@
-import shutil, streamlit, os
+import os, shutil, streamlit
 
 static = os.path.join(os.path.dirname(streamlit.__file__), "static")
-shutil.copy("assets/favicon.png", os.path.join(static, "favicon.png"))
+
+if os.path.exists("assets/favicon.png"):
+    shutil.copy("assets/favicon.png", os.path.join(static, "favicon.png"))
 
 index = os.path.join(static, "index.html")
 html = open(index).read().replace("<title>Streamlit</title>", "<title>AgroTech Intelligence</title>")
